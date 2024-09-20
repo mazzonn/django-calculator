8a9dd462cd8ea869bf646676fe9044559eb60ea1
Mazon <mdzonov2@gmail.com>
2024-09-20 13:07:00 +0300

    goida

---
+++ calc/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    if request.method == 'POST':
+        button_value = request.POST.get('button')
+
+        if button_value == 'AC':
+            new_value = ''
+        elif button_value == '=':
+            current_value = request.POST.get('current_value', '')
+            new_value = eval(str(current_value))
+        else:
+            current_value = request.POST.get('current_value', '')
+            new_value = current_value + button_value
+
+        return render(request, 'home.html', {'input_value': new_value})
+
+    return render(request, 'home.html', {'input_value': ''})
