12ded4b6cd264800b7195489ace1633ae38246bd
Mazon <mdzonov2@gmail.com>
2024-09-20 18:57:15 +0300

    Error exeption

--- calc/views.py
+++ calc/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    if request.method == 'POST':
-        button_value = request.POST.get('button')
+    try:
+        if request.method == 'POST':
+            button_value = request.POST.get('button')
 
-        if button_value == 'AC':
-            new_value = ''
-        elif button_value == '=':
-            current_value = request.POST.get('current_value', '')
-            new_value = eval(str(current_value))
-        else:
-            current_value = request.POST.get('current_value', '')
-            new_value = current_value + button_value
+            if button_value == 'AC':
+                new_value = ''
+            elif button_value == '=':
+                current_value = request.POST.get('current_value', '')
+                new_value = eval(str(current_value))
+            else:
+                current_value = request.POST.get('current_value', '')
+                new_value = current_value + button_value
 
-        return render(request, 'home.html', {'input_value': new_value})
+            return render(request, 'home.html', {'input_value': new_value})
+    except:
+        return render(request, 'home.html', {'input_value': 'Error'})
 
     return render(request, 'home.html', {'input_value': ''})
